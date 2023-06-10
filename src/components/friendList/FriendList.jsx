8c527eeceed7d4df756731f055cd6947bb6a8f46
OleksandrDomin <92186960+OleksandrDomin@users.noreply.github.com>
2023-06-10 22:01:03 +0200

    2 ver
    2 ver

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,24 +1,21 @@
 
 import PropTypes from "prop-types";
 import css from "./FriendList.module.css"
+import FriendListItem from "../friendListItem/FriendListItem";
 
 function FriendList({friends}) {
     return (
-<ul className={css.friend_list}>
-            {friends.map(friends => (
-            <li key={friends.id} className={css.item}>
-            <span className={friends.isOnline ? css.status_online : css.status_offline }></span>
-            <img className={css.avatar} src={friends.avatar} alt={friends.name} width="80" />
-<p className={css.name}>{friends.name}</p></li>))}       
-</ul>
-)
+        <>
+        <ul className={css.friend_list}>
+        {friends.map(friend =>
+        <FriendListItem key={friend.id} {...friend} />
+        )}
+        </ul> 
+        </>)
 };
 
 FriendList.protoType = {
-    id:PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.number.isRequired,
-    isOnline: PropTypes.bool.isRequired
+friends: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number.isRequired }))
 };
 
 
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,13 +1,7 @@
 import PropTypes from "prop-types";
-import css from "./statistics.module.css"
+import css from "./statistics.module.css";
+import getRandomHexColor from "../../utils/HetRandomHexColor";
 
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}; 
-   
 function Statistics({stats, title}) {
   return (
     <section className={css.statistics}>
